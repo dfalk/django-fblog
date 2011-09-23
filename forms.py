13cf61b2f69ccf2d0d943ecf2f22b014860b622b
dfalk <tfalc@mail.ru>
2011-09-23 16:22:01 +0400

    add files for frontend helpers

---
+++ forms.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django import forms
+from fblog.models import Entry
+
+class EntryAdminForm(forms.ModelForm):
+    #content = forms.CharField() # override widget
+
+    class Meta:
+        model = Entry
